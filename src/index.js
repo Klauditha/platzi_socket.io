0c2f6447d3a499ae9b7ddc21d5794820ea0b7a93
Claudia Villarroel <claudia.villarroel@live.com>
2024-01-09 12:17:46 -0300

    :hammer: Broadcast de eventos

--- src/index.js
+++ src/index.js
@@ -1,72 +1,24 @@
-const express = require('express');
-const { createServer } = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
-app.use(express.static(path.join(__dirname, 'views')));
+app.use( express.static(path.join(__dirname, "views")) );
 
-const socketsOnline = [];
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-io.on('connection', (socket) => {
-  socketsOnline.push(socket.id);
+io.on("connection", socket => {
 
-  //Emisión básica
-  socket.emit(
-    'welcome',
-    (data) => console.log(data),
-    'Ahora estas conectado 😊'
-  );
+    socket.on("circle position", position => {
+        socket.broadcast.emit("move circle", position); //amite a todos menos ami
+    });
 
-  socket.on('server', (data) => {
-    console.log(data);
-  });
-
-  //emision a todos
-  io.emit('everyone', socket.id + ' se ha conectado');
-
-  //Emisión a uno solo
-  socket.on('last', (message) => {
-    const lastSocket = socketsOnline[socketsOnline.length - 1];
-    io.to(lastSocket).emit('salute', message);
-  });
-
-  // on , once y off
-  socket.emit("on","holi");
-  socket.emit("on","holi");
-  socket.emit("on","holi");
-
-  socket.emit("once","holi2");
-  socket.emit("once","holi2");
-
-  socket.emit("off","holi3");
-  setTimeout(() => {
-    socket.emit("off","holi3");
-  }, 3000);
-  /*
-  
-  socket.on("on", () => {
-    console.log("Se emite varias veces");
-  })
-  */
-
-  /*
-  console.log("Clientes conectados: " + io.engine.clientsCount);
-  console.log("ID del socket conectado: " + socket.id);
-  socket.on('disconnect', () => {
-    console.log("El socket " + socket.id + " se ha desconectado");
-  });
-
-  socket.conn.once("upgrade", () => {
-    console.log("Hemos pasado de HTTP Long-Polling a", socket.conn.transport.name);
-  });*/
 });
 
 httpServer.listen(3000);
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,75 +1,30 @@
 const socket = io();
 
-socket.on('welcome', (data) => {
-  //console.log(data);
-  text.textContent = data;
+const circle = document.querySelector('#circle');
+
+const drawCircle = (position) => {
+  circle.style.top = position.top;
+  circle.style.left = position.left;
+};
+
+const drag = (e) => {
+  const position = {
+    top: e.clientY + 'px',
+    left: e.clientX + 'px',
+  };
+
+  drawCircle(position);
+  socket.emit('circle position', position);
+};
+
+document.addEventListener('mousedown', (e) => {
+  document.addEventListener('mousemove', drag);
 });
 
-const emitToServer = document.querySelector('#emit-to-server');
-emitToServer.addEventListener('click', () => {
-  socket.emit('server', 'Hola, servidor 😀');
+document.addEventListener('mouseup', (e) => {
+  document.removeEventListener('mousemove', drag);
 });
 
-socket.on('everyone', message => {
-    console.log(message);
+socket.on('move circle', (position) => {
+  drawCircle(position);
 });
-
-const emitToLast = document.querySelector('#emit-to-last');
-emitToLast.addEventListener('click', () => {
-    socket.emit("last","Hola 😊");
-});
-
-socket.on("salute", message => {
-    console.log(message);
-});
-
-//on , once , off
-socket.on("on",() => {
-    console.log("Se emite varias veces");
-});
-
-socket.once("once",() => {
-    console.log("Se emite una sola vez");
-});
-
-const listener = () => {
-    console.log("Se apaga el evento");
-}
-
-socket.on("off",listener);
-
-setTimeout(() => {
-    socket.off("off",listener);
-}, 2000);
-
-/*
-function checkSocketStatus() {
-    if (socket.connected) {
-        console.log("El socket se encuentra conectado");
-    } else {
-        console.log("El socket no se encuentra conectado");
-    }
-}
-
-socket.on('connect', () => {
-    console.log("Cliente: El socket " + socket.id + " se ha conectado");
-    checkSocketStatus();
-});
-
-socket.on('connect_error', () => {
-    console.log("No pude conectarme al servidor 😢");
-});
-
-socket.on('disconnect', () => {
-    console.log("Cliente: El socket " + socket.id + " se ha desconectado");
-    checkSocketStatus();
-});
-
-socket.io.on('reconnect_attempt', () => {
-    console.log("Estoy intentando reconectar 😍");
-});
-
-socket.io.on('reconnect', () => {
-    console.log("Me he reconectado 😀");
-})
-*/
